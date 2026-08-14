08b87c335119647ceed5b0e74edec87cdc9c3e4f
lahlan-h <106949397+lahlan-h@users.noreply.github.com>
2026-08-15 00:08:42 +1000

    add iphone scaffold

---
+++ apps/mobile/src/app/index.tsx
@@ -0,0 +1,20 @@
+import { Link } from "expo-router";
+import { StyleSheet, Text, View } from "react-native";
+
+export default function Index() {
+  return (
+    <View style={styles.container}>
+      <Text>Welcome to Expo Router!</Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flexDirection: "row",
+    alignItems: "center",
+    justifyContent: "center",
+    backgroundColor: "#eeeeee",
+    gap: 10,
+  },
+});
