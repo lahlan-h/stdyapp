21fafff8faa41979fa8d9a2194daf16ed81b49ef
lahlan-h <106949397+lahlan-h@users.noreply.github.com>
2026-09-15 14:37:12 +1000

    change file name of ThemeProvider.tsx to useTheme.tsx

--- apps/mobile/src/theme/index.ts
+++ apps/mobile/src/theme/index.ts
@@ -1,4 +1,4 @@
-export { useTheme, ThemeProvider, useStyles, type ThemePreference } from "./ThemeProvider";
+export { useTheme, ThemeProvider, useStyles, type ThemePreference } from "./useTheme";
 export { lightColors, darkColors, type ColorScheme } from "./colors";
 export { ROW_ICON_SIZE } from "./settings.styles";
 // find useHomeStyles
